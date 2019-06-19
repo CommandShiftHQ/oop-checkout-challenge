0d020889b8c11377a50ab0dc50ba75251aae4787
Ian King <ian.antking@gmail.com>
2019-06-19 21:09:49 +0100

    added add to basket method

--- __tests__/checkout.test.js
+++ __tests__/checkout.test.js
@@ -5,6 +5,10 @@ const catalogue = [
     barcode: 123,
     name: 'apple',
   },
+  {
+    barcode: 456,
+    name: 'banana',
+  },
 ];
 
 describe('checkout object', () => {
@@ -19,10 +23,25 @@ describe('checkout object', () => {
     it('has a catalogue', () => {
       expect(checkout).toHaveProperty('catalogue');
     });
+    it('has a basket', () => {
+      expect(checkout).toHaveProperty('basket');
+    });
+    it('the basket is an array', () => {
+      expect(checkout.basket).toEqual([]);
+    });
   });
   describe('scan method', () => {
     it('takes a barcode and returns an item', () => {
       expect(checkout.scan(123)).toBeInstanceOf(Object);
+    });
+    it('returns the correct item', () => {
+      expect(checkout.scan(123)).toBe(catalogue[0]);
+    });
+  });
+  describe('add to basket method', () => {
+    it('adds a scanned item to the basket', () => {
+      checkout.addToBasket(catalogue[0]);
+      expect(checkout.basket).toEqual([catalogue[0]]);
     });
   });
 });
--- src/checkout.js
+++ src/checkout.js
@@ -1,11 +1,15 @@
 /* eslint-disable func-names */
 function Checkout(catalogueData) {
   this.catalogue = catalogueData;
+  this.basket = [];
 }
 
 Checkout.prototype = {
   scan: function (barcode) {
     return this.catalogue.find(item => item.barcode === barcode);
+  },
+  addToBasket: function (item) {
+    this.basket.push(item);
   },
 };
 
